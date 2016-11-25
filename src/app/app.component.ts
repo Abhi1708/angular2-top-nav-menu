09ac4db24ea8af3531a0ec863d9ccce7847b9ea3
abhilash Telaganeedi <abhilash Telaganeedi>
2016-11-25 03:44:19 -0500

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,39 +19,6 @@ export class AppComponent {
       text: 'View COntract Pipeline'
     }
   ];
-  dropdownLoan: Array<any> = [
-    {
-      text: 'Take out Cash Contract',
-    },
-    {
-      text: 'Remove Loans from Contract',
-    },
-    {
-      text: 'Import Guarantor Contracts'
-    }
-  ];
-  dropdownBill: Array<any> = [
-    {
-      text: 'Form 1034E',
-    },
-    {
-      text: 'Note Delivery Cover Sheet',
-    },
-    {
-      text: 'Bill of Sale ID'
-    }
-  ];
-  dropdownContract: Array<any> = [
-    {
-      text: 'Take of Guarantor Contract',
-    },
-    {
-      text: 'Pair of Contract',
-    },
-    {
-      text: 'View Import Contract Results'
-    }
-  ];
   toggleDropdown() {
     this.isOpen = !this.isOpen;
   }
